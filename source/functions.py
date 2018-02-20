a8b2baf9479895fe181e6345044cb3375d121b82
Axeleik <alexmats09@googlemail.com>
2018-02-20 16:01:19 +0100

    new approach, we now evaluate the features and try possible combinations

--- source/functions.py
+++ source/functions.py
@@ -3,6 +3,7 @@ from scipy import signal
 import scipy.io.wavfile as sci_wav
 import os
 import librosa
+import librosa.feature as feat
 
 def import_sounds(path_to_sound_files):
     """
@@ -24,10 +25,58 @@ def import_sounds(path_to_sound_files):
 
     return sound_waves,sound_types
 
-def extract_features(soundwaves):
+def extract_features(soundwave,sampling_rate,feature_list=[]):
 
+    if len(feature_list)==0:
+        feature_list=["chroma_stft","chroma_cqt","chroma_cens","melspectrogram",
+                      "mfcc","rmse","spectral_centroid","spectral_bandwidth",
+                      "spectral_contrast","spectral_flatness","spectral_rolloff",
+                      "poly_features","tonnetz","zero_crossing_rate"]
 
     features=[]
+
+    if "chroma_stft" in features:
+        features.append(feat.chroma_stft(soundwave, sampling_rate))
+
+    if "chroma_cqt" in features:
+        features.append(feat.chroma_cqt(soundwave, sampling_rate))
+
+    if "chroma_cens" in features:
+        features.append(feat.chroma_cens(soundwave, sampling_rate))
+
+    if "melspectrogram" in features:
+        features.append(feat.melspectrogram(soundwave, sampling_rate))
+
+    if "mfcc" in features:
+        features.append(feat.mfcc(soundwave, sampling_rate))
+
+    if "rmse" in features:
+        features.append(feat.rmse(soundwave))
+
+    if "spectral_centroid" in features:
+        features.append(feat.spectral_centroid(soundwave, sampling_rate))
+
+    if "spectral_bandwidth" in features:
+        features.append(feat.spectral_bandwidth(soundwave, sampling_rate))
+
+    if "spectral_contrast" in features:
+        features.append(feat.spectral_contrast(soundwave, sampling_rate))
+
+    if "spectral_flatness" in features:
+        features.append(feat.spectral_flatness(soundwave))
+
+    if "spectral_rolloff" in features:
+        features.append(feat.spectral_rolloff(soundwave, sampling_rate))
+
+    if "poly_features" in features:
+        features.append(feat.poly_features(soundwave, sampling_rate))
+
+    if "tonnetz" in features:
+        features.append(feat.tonnetz(soundwave, sampling_rate))
+
+    if "zero_crossing_rate" in features:
+        features.append(feat.zero_crossing_rate(soundwave))
+
 
     return features
 
--- testing/open_files.py
+++ testing/open_files.py
@@ -12,13 +12,15 @@ import librosa.feature as feat
 def import_sounds(path_to_sound_files):
 
 
+
+
     # 1. get sound file names and 2. names of types of sounds
     sound_file_names=os.listdir(path_to_sound_files)
     sound_types=[name[4:-4] for name in sound_file_names]
 
 
     #getting test sound
-    sci_sr,sci_y=sci_wav.read(path_to_sound_files+sound_file_names[0])
+    sci_sr,sci_y=sci_wav.read(path_to_sound_files+sound_file_names[-1])
     sound_waves, sampling_rate = librosa.load(path_to_sound_files+sound_file_names[0],None)
 
     #processing file
@@ -40,9 +42,6 @@ def import_sounds(path_to_sound_files):
     tonnetz=feat.tonnetz(sound_waves,sampling_rate)
     zero_crossing_rate=feat.zero_crossing_rate(sound_waves)
 
-
-
-    librosa.feature.mfcc()
 
 
     return sound_waves,sound_types
